fc62d5e828ab84bf2df3180c21073a5f3f86b6da
emenay <57572123+emenay@users.noreply.github.com>
2021-11-15 17:26:55 -0500

    test

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,8 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-    i = a + b;
+    i = new Number
+    i = a + b
     console.log({a} + " + " + {b} + " = " + {i});
 }
 
