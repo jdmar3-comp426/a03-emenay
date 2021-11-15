b51ba5ee1b3d3d0b7487bae37a499c0df223b193
emenay <57572123+emenay@users.noreply.github.com>
2021-11-15 18:21:12 -0500

    mild 1 3

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -41,8 +41,8 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let min = Math.min(numbers);
-    let max = Math.max(numbers);
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
     return {max, min}
 }
 
