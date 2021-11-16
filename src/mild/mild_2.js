e4bcd434ee864a1eff10756c6c98da0bdbe52e18
emenay <57572123+emenay@users.noreply.github.com>
2021-11-15 19:35:43 -0500

    mild 2 3

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -70,7 +70,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   const o = [...object]
+   o.removeKey(key)
+   return o
 }
 
 /**
