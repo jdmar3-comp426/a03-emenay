2784d4f668c9d5bd138a3eea8362c7fd029ca646
emenay <57572123+emenay@users.noreply.github.com>
2021-12-02 19:44:53 -0500

    spicy 2-4

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -47,7 +47,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-    return function(num2){num1*num2}
+    return function(num2){
+        num1*num2
+    }
 };
 
 
