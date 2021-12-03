ef6a5b9ed27b6662ddae6366df0e8cc5e10b1b65
emenay <57572123+emenay@users.noreply.github.com>
2021-12-03 15:02:02 -0500

    43

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -56,9 +56,11 @@ export function searchMpg(car_data, minCity, minHighway) {
  * @returns {[]} array of cars
  */
 export function searchName(car_data, searchTerm) {
-    var a = car_data.filter(object => object.id == searchTerm)
+    var a = car_data.filter(object => object.id.toLowerCase().includes(searchTerm.toLowerCase()))
     return a
 }
+
+
 
 
 /**
