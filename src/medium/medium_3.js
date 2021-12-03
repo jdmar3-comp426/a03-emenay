4fa273b55c2171dac6dea2c8a81a1dba23d8f542
emenay <57572123+emenay@users.noreply.github.com>
2021-12-02 22:17:05 -0500

    37

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -56,7 +56,7 @@ export function searchMpg(car_data, minCity, minHighway) {
  * @returns {[]} array of cars
  */
 export function searchName(car_data, searchTerm) {
-
+    return car_data.filter(o => o.id === searchTerm)
 }
 
 
